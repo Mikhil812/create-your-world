2feb372f6fa3aaa0bf65fe4eb9d4df72862a8481
Mikhil Angre <mikhilangre812@gmail.com>
2026-01-27 16:46:40 +0530

    Made a new branch to experiment around with

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,25 +1,88 @@
-import { useEffect, useRef } from "react";
+import {useEffect, useRef, useState} from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import continents from "./data/continents.geojson"
 
-export default function Map() {
+export default function Map(){
   const mapContainer = useRef(null);
 
-  useEffect(() => {
+  const[coords, setCoords] = useState(null);
+  const markerRef = useRef(null);
+
+  useEffect(()=> {
     const map = new maplibregl.Map({
       container: mapContainer.current,
       style: "https://demotiles.maplibre.org/style.json",
-      center: [0, 20], // longitude, latitude
+      center: [0, 20],
       zoom: 1.5,
     });
 
-    return () => map.remove();
+    map.on("load", () => {
+      map.addSource("continents", {
+        type: "geojson",
+        data: continents,
+      });
+
+      map.addLayer({
+        id: "continent-borders",
+        type: "line",
+        source: "continents",
+        paint: {
+          "line-color": "#000",
+          "line-width": 2,
+        },
+      });
+    });
+
+    // Click Event Handler
+    const handleClick = (event) => {
+      const {lng, lat} = event.lngLat;
+      const zoom = map.getZoom();
+
+      setCoords({lng, lat});
+
+      if(markerRef.current){
+        markerRef.current.remove();
+      }
+      markerRef.current = new maplibregl.Marker()
+        .setLngLat([lng, lat])
+        .addTo(map);
+    }
+    map.on("click", handleClick);
+
+    return() => {
+      if (markerRef.current) {
+        markerRef.current.remove();
+      }
+      map.off("click", handleClick);
+      map.remove();
+    };
   }, []);
 
   return (
-    <div
-      ref={mapContainer}
-      style={{ width: "100%", height: "100%" }}
-    />
+    <>
+      <div 
+        ref={mapContainer}
+        style={{width:"100%", height: "100%"}}
+      />
+      {coords && (
+        <div
+          style={{
+            position: "absolute",
+            bottom: 20,
+            left: 20,
+            background: "white",
+            padding: "8px 12px",
+            borderRadius: "4px",
+            fontSize: "14px",
+          }}
+        >
+          <div><strong>Longitude:</strong> {coords.lng.toFixed(4)}</div>
+          <div><strong>Latitude:</strong> {coords.lat.toFixed(4)}</div>
+        </div>
+      )}
+    </>
   );
 }
+
+
